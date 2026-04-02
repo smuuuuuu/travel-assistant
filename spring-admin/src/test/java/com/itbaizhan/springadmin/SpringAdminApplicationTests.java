af07451329b9ced44c7e8caddcefd6fe70e3d6be
baizhan <baizhan@163.com>
2026-04-02 15:59:24 +0800

    travel-admin

---
+++ spring-admin/src/test/java/com/itbaizhan/springadmin/SpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itbaizhan.springadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
